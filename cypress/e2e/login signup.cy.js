2794fba0a2500c13f08747a88d7b7197d9e4d14c
Nfoye Daryl <maviance.daryl.nfoye@gmail.com>
2025-01-20 12:36:34 +0100

    commented my code

--- cypress/e2e/login signup.cy.js
+++ cypress/e2e/login signup.cy.js
@@ -4,13 +4,19 @@ describe('Automation exercises login and sign up', () => {
       cy.visit('https://automationexercise.com')
   })
 
+  // Test case for signing up a new user
   it.only('signs up a new user', () => {
+    // Navigate to the login page
     cy.getByHref("/login").click()
+    // Verify the signup form exists
     cy.get(`[action='/signup']`).should("exist")
+    // Fill out the signup form
     cy.getByData("signup-name").type("Daryl  Nfoye")
     cy.getByData("signup-email").type("ewilde@gmail.com")
     cy.getByData("signup-button").click()
+    // Verify that the follow up additional details form appears
     cy.get(`[action='/signup']`).should("exist")
+    // Fill out additional details
     cy.getByValue("Mr").click()
     cy.getByData("password").type("Mypassword")
     cy.getByData("days").type("01")
@@ -28,19 +34,27 @@ describe('Automation exercises login and sign up', () => {
     cy.getByData("city").type("Douala")
     cy.getByData("zipcode").type("00237")
     cy.getByData("mobile_number").type("699255753")
+    // Submit the signup form
     cy.getByData("create-account").click()
+    // Verify the continue button exists and click it
     cy.getByData("continue-button").should("exist").click()
   })
 
+  // Test case for logging in a user and deleting the account
   it("logs in a user and deletes his account", () => {
+    // Navigate to the login page
     cy.getByHref("/login").click()
+    // Verify the login form exists
     cy.get(`[action='/login']`).should("exist")
+    // Fill out the login form
     cy.getByData("login-email").type("nfoyedewilde@gmail.com")
     cy.getByData("login-password").type("Mypassword")
+    // Submit the login form
     cy.getByData("login-button").should("exist").click()
+    // CLick on the delete account button
     cy.getByHref("/delete_account").click()
+    // Verify the continue button exists and click it
     cy.getByData("continue-button").should("exist").click()
   })
 
 })
-
--- cypress/e2e/veriify login.cy.js
+++ cypress/e2e/veriify login.cy.js
@@ -1,16 +1,22 @@
-describe("Verify  logged in user", () => {
+describe("Verify logged in user", () => {
 
     beforeEach(() => {
         cy.visit('https://automationexercise.com')
     })
 
-    it("logs in use and verify", () => {
+    it("logs in the user and verifies that his name is displayed in the Menu", () => {
+        // Verify that the current pathname is the one of the home page
         cy.location("pathname").should("eq","/")
+        // Click on the login option
         cy.getByHref("/login").click()
+        // Ensure the login form is present
         cy.getByClass("login-form").should("exist")
+        // Fill out the login form
         cy.getByData("login-email").type("ewilde@gmail.com")
         cy.getByData("login-password").type("Mypassword")
-        cy.getByData("login-button").should("exist").click()
+        // Click the login button
+        cy.getByData("login-button").click()
+        // Verify that the user's name is displayed in the menu
         cy.get("a").contains("Logged in as Daryl Nfoye").should("exist")
     })
 
